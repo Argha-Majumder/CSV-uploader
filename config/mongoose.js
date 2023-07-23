039c3ffa46586380a7b254cd4f7eb9a59526cd5a
Argha-Majumder <majumderargha847@gmail.com>
2023-07-23 16:14:46 +0530

    minor changes

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // creating the database on the cloud
-const DB = "mongodb://argha:argha234@ac-oaaqjwl-shard-00-00.0wvn1g6.mongodb.net:27017,ac-oaaqjwl-shard-00-01.0wvn1g6.mongodb.net:27017,ac-oaaqjwl-shard-00-02.0wvn1g6.mongodb.net:27017/csv-upload?ssl=true&replicaSet=atlas-cu1ow8-shard-0&authSource=admin&retryWrites=true&w=majority";
+const DB = process.env.DATABASE_URL;
 
 // creating the connection
 mongoose.connect(DB).then(()=> {
